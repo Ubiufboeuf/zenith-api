647298e1561f61fe4e76d2f3c03734edff90a59e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-06-14 21:35:35 -0300

    fix(payments): arregla el tipo 'Currency' y sus posibles valores

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ export const {
   PORT = 1234
 } = process.env
 
-export const CURRENCIES = ['$', 'U$S', '€'] as const
+export const CURRENCIES = ['UYU', 'USD', 'EUR'] as const // ISO 4217
 export const SALE_STATUS = ['CANCELLED', 'PENDING', 'COMPLETED'] as const
 export const PAYMET_STATUS = ['PAID', 'PARTIAL', 'OWED', 'REFUNDED', 'VOIDED'] as const
 
--- src/types/paymentsTypes.ts
+++ src/types/paymentsTypes.ts
@@ -2,6 +2,6 @@ import type { CURRENCIES } from '@/lib/constants'
 import type { createSalePaymentSchema } from '@/schemas/paymentsSchemas'
 import type z from 'zod'
 
-export type Currency = keyof typeof CURRENCIES
+export type Currency = typeof CURRENCIES[number]
 
 export type CreateSalePayment = z.infer<typeof createSalePaymentSchema>
