bdfbc280afa0191304f8bfee862a3cc6547e832a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-06-25 15:45:01 -0300

    feat(products): agrega un esquema para los códigos de los productos y crea un tipo en base a él

--- src/schemas/productsSchemas.ts
+++ src/schemas/productsSchemas.ts
@@ -1,8 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import z from 'zod'
 import { ProductCodesRowSchema, ProductEventsRowSchema, ProductsRowSchema } from './db'
 
+export const ProductCodeSchema = ProductCodesRowSchema.extend({
+  is_main: z.boolean().catch((ctx: any) => {
+    if (!ctx) return false
+    if (typeof ctx === 'number') return ctx === 1
+    return ctx === '1'
+  })
+})
+
 export const ProductSchema = ProductsRowSchema.extend({
-  codes: z.array(ProductCodesRowSchema)
+  codes: z.array(ProductCodeSchema)
 })
 
 export const ProductFullSchema = ProductSchema.extend({
--- src/types/productsTypes.ts
+++ src/types/productsTypes.ts
@@ -1,5 +1,6 @@
-import type { ProductFullSchema, ProductSchema } from '@/schemas/productsSchemas'
+import type { ProductCodeSchema, ProductFullSchema, ProductSchema } from '@/schemas/productsSchemas'
 import type z from 'zod'
 
 export type Product = z.infer<typeof ProductSchema>
 export type ProductFull = z.infer<typeof ProductFullSchema>
+export type ProductCode = z.infer<typeof ProductCodeSchema>
