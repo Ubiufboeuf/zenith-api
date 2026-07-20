8d2399ba153d05560126e587de8ac345db2412e4
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-07-20 18:16:42 -0300

    feat(lib): agrega normalizadores y mejora los indexadores de arrays
    los normalizadores agarran un objeto y devuelven otro con un tipo específico

--- src/lib/indexArray.ts
+++ src/lib/indexArray.ts
@@ -1,13 +1,54 @@
-export function indexArray<T> (array: T[], key: keyof T): Map<string, T[]> {
+import type { ProductCode, ProductEvent } from '@/types/productsTypes'
+import type { Row } from '@libsql/client'
+import { normalizeCodeRow, normalizeEventRow } from './normalizers'
+
+interface IndexOptions {
+  filter: 'keepAll' | 'onNullishIgnore' | 'onFalsyIgnore'
+}
+
+const DefaultOptions: IndexOptions = {
+  filter: 'keepAll'
+}
+
+export function indexArray<T, R = T> (
+  array: T[],
+  key: keyof T,
+  transform: ((item: T) => R) | null = null,
+  options: IndexOptions = DefaultOptions
+): Map<string, R[]> {
   return array.reduce((map, item) => {
-    const id = String(item[key]) // Con esto debería funcionar también con símbolos y números
+    const id = String(item[key])
 
     if (!map.has(id)) {
       map.set(id, [])
     }
-      
-    map.get(id)!.push(item as T)
+    
+    const valueToAdd = transform ? transform(item) : item as unknown as R
+
+    if (options.filter === 'onFalsyIgnore' && !valueToAdd) return map
+    if (options.filter === 'onNullishIgnore' && valueToAdd == null) return map
+    
+    map.get(id)!.push(valueToAdd)
     
     return map
-  }, new Map<string, T[]>())
+  }, new Map<string, R[]>())
 }
+
+export function indexCodes (codesRows: Row[]): Map<string, ProductCode[]> {
+  return indexArray(
+    codesRows as unknown as ProductCode[],
+    'product_id',
+    (item) => item.id ? normalizeCodeRow(item) : undefined as unknown as ProductCode,
+    { filter: 'onNullishIgnore' }
+  )
+}
+
+export function indexEvents (eventsRows: Row[]): Map<string, ProductEvent[]> {
+  return indexArray(
+    eventsRows as unknown as ProductEvent[],
+    'product_id',
+    (item) => item.id ? normalizeEventRow(item) : undefined as unknown as ProductEvent,
+    { filter: 'onNullishIgnore' }
+  )
+}
+
---
+++ src/lib/normalizers.ts
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ProductEventsRowSchema } from '@/schemas/db'
+import { ProductCodeSchema } from '@/schemas/productsSchemas'
+import type { ProductCode, ProductEvent } from '@/types/productsTypes'
+import type { Row } from '@libsql/client'
+
+type Strictness = 'high' | 'low' | 'none'
+
+export function normalizeCodeRow (item: any, strictness?: 'none'): any
+export function normalizeCodeRow (item: any, strictness?: 'low'): ProductCode
+export function normalizeCodeRow (item: any, strictness?: 'high'): ProductCode | undefined
+
+export function normalizeCodeRow (item: any, strictness: Strictness = 'low') {
+  let value
+
+  if (strictness === 'none') value = item as any
+  if (strictness === 'low') value = item as Row
+  if (strictness === 'high') {
+    const validation = ProductCodeSchema.safeParse(item)
+    if (!validation.success) return
+    value = validation.data
+  }
+  
+  return {
+    id: value.id ?? value.code_id,
+    product_id: value.product_id,
+    code: value.code,
+    type: value.type,
+    is_main: value.is_main
+  }
+}
+export function normalizeEventRow (item: any, strictness?: 'none'): any
+export function normalizeEventRow (item: any, strictness?: 'low'): ProductEvent
+export function normalizeEventRow (item: any, strictness?: 'high'): ProductEvent | undefined
+
+export function normalizeEventRow (item: Row, strictness: Strictness = 'low') {
+  let value
+
+  if (strictness === 'none') value = item as any
+  if (strictness === 'low') value = item as Row
+  if (strictness === 'high') {
+    const validation = ProductEventsRowSchema.safeParse(item)
+    if (!validation.success) return
+    value = validation.data
+  }
+
+  return {
+    id: value.id ?? value.code_id,
+    product_id: value.product_id,
+    created_at: value.created_at,
+    event_type: value.event_type,
+    new_value: value.new_value,
+    previous_value: value.previous_value
+  }
+}
